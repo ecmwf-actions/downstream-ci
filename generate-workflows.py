3a84e620d743f5c2bb5bd2864c5f6b10673efec5
Iain Russell <40060766+iainrussell@users.noreply.github.com>
2024-08-20 15:19:46 +0100

    Add concurrency setting to downstream-ci

--- generate-workflows.py
+++ generate-workflows.py
@@ -127,7 +127,7 @@ class Workflow:
 
     def concurrency(self) -> object:
         c = {
-            "group": "${{ github.workflow }}-${{ github.ref }}",
+            "group": "${{ github.workflow }}-${{ github.ref }}-" + self.name,
             "cancel-in-progress": True,
         }
 
